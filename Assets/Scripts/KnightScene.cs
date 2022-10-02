7aafaa4ae55dd119d604ee4e108c55353caf75f1
WillYum <liamshere98@gmail.com>
2022-10-02 09:27:56 +0300

    Cool map

--- Assets/Scripts/KnightScene.cs
+++ Assets/Scripts/KnightScene.cs
@@ -5,12 +5,21 @@ using Utils.GenericSingletons;
 
 public class KnightScene : MonoBehaviourSingleton<KnightScene>
 {
+    [SerializeField] private ParticleSystem endPoint;
 
+
+    private void Awake()
+    {
+        endPoint.gameObject.SetActive(false);
+    }
     public void OnKillKnight()
     {
         var BattleCards_UI = GameObject.Find("BattleCards_UI");
         BattleCards_UI.SetActive(false);
 
         MainCharacter.instance.DisableAllAbilityVisuals();
+
+        endPoint.gameObject.SetActive(true);
+        endPoint.Play();
     }
 }
